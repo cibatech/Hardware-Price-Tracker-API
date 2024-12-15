720f6ecf47132a755fb5678660e90f3e0181bdb8
ciringa <thierrird957468@gmail.com>
2024-12-14 21:43:35 -0300

    admin panel table finished

--- src/http/Controllers/Scrap/GETScrapListByDateController.ts
+++ src/http/Controllers/Scrap/GETScrapListByDateController.ts
@@ -2,12 +2,13 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { GetScrapListByDateUseCase } from "../../../services/Scrap/GetScrapListByDate";
 import { PrismaScrapRepository } from "../../../repository/Prisma/PrismaScrapRepository";
+import { PrismaIssuesRepository } from "../../../repository/Prisma/PrismaIssuesRepository";
 
 export async function GETScrapListByDateController(req:FastifyRequest,res:FastifyReply){
     const {PasDays} = z.object({
         PasDays:z.string()
     }).parse(req.params)
-    const Service = new GetScrapListByDateUseCase(new PrismaScrapRepository)
+    const Service = new GetScrapListByDateUseCase(new PrismaScrapRepository, new PrismaIssuesRepository)
 
     try{
         const response = await Service.execute(Number(PasDays))
--- src/services/Scrap/GetScrapListByDate.ts
+++ src/services/Scrap/GetScrapListByDate.ts
@@ -1,26 +1,35 @@
 import { Scrap } from "../../../prisma/deploy-output";
+import { IssuesRepository } from "../../repository/IssueRepository";
 import { ScrapRepository } from "../../repository/ScrapRepository";
+
+
+interface ScrapReturnCode{
+    succeeded:number,failed:number
+}
 interface GetScrapListByDateResponse{
     ScrapList:Scrap[],
-    ScrapInfo:Record<string,number>
+    ScrapInfo:Record<string,ScrapReturnCode>
 }
 
 export class GetScrapListByDateUseCase {
-    constructor(private ScrapRepo:ScrapRepository){}
+    constructor(private ScrapRepo:ScrapRepository, private IssuesRepo:IssuesRepository){}
     async execute(PasDays:number):Promise<GetScrapListByDateResponse>{
         // Chama o método findByDate para obter os dados filtrados
         const scrapData = await this.ScrapRepo.findByDate(PasDays);
-        const ScrapReturn = scrapData
+
+        const IssuesDate = await this.IssuesRepo.findByDate(PasDays)
+
         // Obtém o ano atual
         const anoAtual = new Date().getFullYear();
 
+        console.log("got there")
         // Inicializa o dicionário com os meses
         const meses = [
             "Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
             "Jul", "Ago", "Set", "Out", "Nov", "Dez"
         ];
-        const resultado = meses.reduce<Record<string, number>>((acc, mes) => {
-            acc[mes] = 0; // Inicializa cada mês com 0
+        const resultado = meses.reduce<Record<string,ScrapReturnCode >>((acc, mes) => {
+            acc[mes] = {succeeded:0,failed:0}; // Inicializa cada mês com 0
             return acc;
         }, {});
 
@@ -30,13 +39,23 @@ export class GetScrapListByDateUseCase {
             const anoItem = createdAt.getFullYear();
             if (anoItem === anoAtual) {
                 const mes = meses[createdAt.getMonth()]; // Obtém o mês pelo índice
-                resultado[mes] += 1; // Incrementa a contagem para o mês
+                resultado[mes].succeeded += 1; // Incrementa a contagem para o mês
+            }
+        });
+        //Valores que fracassaram 
+        IssuesDate.forEach(item => {
+            const createdAt = new Date(item.When);
+            const anoItem = createdAt.getFullYear();
+            if (anoItem === anoAtual) {
+                const mes = meses[createdAt.getMonth()]; // Obtém o mês pelo índice
+                resultado[mes].failed += 1; // Incrementa a contagem para o mês
             }
         });
 
         return {
-            ScrapList:ScrapReturn,
-            ScrapInfo:resultado
+            ScrapList:scrapData,
+            ScrapInfo:resultado,
+            
         };
     }
 
